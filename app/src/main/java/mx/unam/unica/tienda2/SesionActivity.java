f36fdedb4ed8c449a2440efee37c1fc3ff3863b1
Armando Rodríguez <jarodriguezvera@gmail.com>
2022-02-28 14:18:41 -0600

    Control de Excepciones, Toast y equals

--- app/src/main/java/mx/unam/unica/tienda2/SesionActivity.java
+++ app/src/main/java/mx/unam/unica/tienda2/SesionActivity.java
@@ -2,32 +2,71 @@ package mx.unam.unica.tienda2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SesionActivity extends AppCompatActivity {
-    EditText edtNombre;
+    private EditText edtNombre;
+    private EditText edtPassword;
+    private Button btnEntrar;
+    private String usuario="";
+    private String password="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sesion);
         edtNombre= findViewById(R.id.edtNombre);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(onClickEntrar);
 
     }
 
-    public  void onClickSplit(View v)
+
+    View.OnClickListener onClickEntrar = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (Valida())
+            {
+                Intent intMain = new Intent(SesionActivity.this,MainActivity.class );
+                startActivity(intMain);
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(),"Usuario o contraseña incorrecta",Toast.LENGTH_LONG).show();
+            }
+
+        }
+    };
+
+
+    private boolean Valida()
     {
-        String texto= edtNombre.getText().toString();
-        String [] datos;
-        datos = texto.split("\\+");
-        for( int i=0;  i< datos.length; i++)
+        try {
+            usuario=edtNombre.getText().toString();
+            password= edtPassword.getText().toString();
+        }
+        catch (Exception error)
         {
-            Log.d("Cadena",datos[i]);
+            Log.d("Login","Error en los dados: "+error.toString() );
         }
 
+        if ( usuario.equals("armando") && password.equals("armando")  )
+        {
+            return true;
+        }
+        else
+        {
+            return  false;
+        }
 
     }
+
+
 }
