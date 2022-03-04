bc83d6f59a1a09225c62cce444a83caeda6b38be
Armando Rodríguez <jarodriguezvera@gmail.com>
2022-03-04 14:09:49 -0600

    Menú funcional en NavButton

--- app/src/main/java/mx/unam/unica/tienda2/MainActivity.java
+++ app/src/main/java/mx/unam/unica/tienda2/MainActivity.java
@@ -1,8 +1,14 @@
 package mx.unam.unica.tienda2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView btmNavigationPrincipal;
+        btmNavigationPrincipal= findViewById(R.id.btmNavigationPrincipal);
+        btmNavigationPrincipal.setOnItemSelectedListener(navListener);
+
     }
+
+    NavigationBarView.OnItemSelectedListener navListener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId())
+            {
+                case R.id.nav_home:
+                    Toast.makeText(getApplicationContext(),"Inicio",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.nav_buscar:
+                    Toast.makeText(getApplicationContext(),"Buscar",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.nav_menu_comprar:
+                    Toast.makeText(getApplicationContext(),"Comprar",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.nav_menu_iniciar:
+                    Toast.makeText(getApplicationContext(),"Sesión",Toast.LENGTH_SHORT).show();
+                    break;
+            }
+
+            return true;
+        }
+    };
+
 }
