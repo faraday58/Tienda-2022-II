3637f71b3671a78feeb50b11ef9ee534e27eeb28
Armando Rodríguez <jarodriguezvera@gmail.com>
2022-03-07 14:36:35 -0600

    Adapter

--- app/src/main/java/mx/unam/unica/tienda2/fragments/InicioFragment.java
+++ app/src/main/java/mx/unam/unica/tienda2/fragments/InicioFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,10 +15,27 @@ import mx.unam.unica.tienda2.R;
 
 public class InicioFragment extends Fragment {
 
+    private ListView lstProductos;
+    private String [] productos;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_inicio,container,false);
+        lstProductos=v.findViewById(R.id.lstProductos);
+        RellenarProductos();
+        RellenarLista();
         return v;
     }
+
+    private void RellenarLista() {
+        ArrayAdapter miAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, productos );
+        lstProductos.setAdapter(miAdapter);
+    }
+
+    private void RellenarProductos() {
+        productos= getResources().getStringArray(R.array.string_productos);
+    }
+
+
+
 }
