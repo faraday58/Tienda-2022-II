d14d168f250275fe7c1a24a770982913da0ab636
Armando Rodríguez <jarodriguezvera@gmail.com>
2022-03-07 14:58:12 -0600

    Menu

--- app/src/main/java/mx/unam/unica/tienda2/fragments/InicioFragment.java
+++ app/src/main/java/mx/unam/unica/tienda2/fragments/InicioFragment.java
@@ -1,11 +1,16 @@
 package mx.unam.unica.tienda2.fragments;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,7 +29,33 @@ public class InicioFragment extends Fragment {
         lstProductos=v.findViewById(R.id.lstProductos);
         RellenarProductos();
         RellenarLista();
+        registerForContextMenu(lstProductos);
         return v;
+    }
+
+    @Override
+    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater= getActivity().getMenuInflater();
+        inflater.inflate(R.menu.menu_contextual_archivos,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info= (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+
+
+        switch (item.getItemId()   )
+        {
+            case R.id.menContCargar:
+                Toast.makeText(getContext(),"Cargando",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menContSalvar:
+                Toast.makeText(getContext(),"Salvando",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onContextItemSelected(item);
     }
 
     private void RellenarLista() {
