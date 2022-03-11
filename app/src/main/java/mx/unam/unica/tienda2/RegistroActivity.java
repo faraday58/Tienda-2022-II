f8c47b9f14de460147364964e53acd27597c9ff6
Armando Rodríguez <jarodriguezvera@gmail.com>
2022-03-10 22:40:25 -0600

    Guardar preferencias y entrar directo

--- app/src/main/java/mx/unam/unica/tienda2/RegistroActivity.java
+++ app/src/main/java/mx/unam/unica/tienda2/RegistroActivity.java
@@ -2,13 +2,54 @@ package mx.unam.unica.tienda2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistroActivity extends AppCompatActivity {
 
+    EditText edtUsuario;
+    EditText edtPassword;
+    EditText edtPasswordConfirmar;
+    Button btnSalvar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        edtUsuario= findViewById(R.id.edtUsuario);
+        edtPassword= findViewById(R.id.edtPassword);
+        edtPasswordConfirmar= findViewById(R.id.edtPasswordConfirmar);
+        btnSalvar= findViewById(R.id.btnSalvar);
+        btnSalvar.setOnClickListener(onClickRegistrar);
     }
+
+
+    View.OnClickListener onClickRegistrar= View ->{
+        if(edtUsuario.getText().toString().equals(""))
+        {
+            Toast.makeText(getApplicationContext(),"Debes ingresar un usuario",Toast.LENGTH_SHORT).show();
+        }
+        else if(edtPassword.getText().toString().equals(edtPasswordConfirmar.getText().toString())  )
+        {
+            Intent intLogin= new Intent(RegistroActivity.this,SesionActivity.class  );
+
+            SharedPreferences preferences= getSharedPreferences(getResources().getString(R.string.strCredenciales), Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor= preferences.edit();
+            String usuario= edtUsuario.getText().toString();
+            String password=edtPassword.getText().toString();
+
+            editor.putString(getResources().getString(R.string.strUser),usuario);
+            editor.putString(getResources().getString(R.string.strPassword),password);
+            editor.commit();
+            startActivity(intLogin);
+            finish();
+        }
+    };
+
+
 }
--- app/src/main/java/mx/unam/unica/tienda2/SesionActivity.java
+++ app/src/main/java/mx/unam/unica/tienda2/SesionActivity.java
@@ -10,11 +10,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SesionActivity extends AppCompatActivity {
     private EditText edtNombre;
     private EditText edtPassword;
+    private TextView txtvRegistrar;
     private Button btnEntrar;
     private String usuario="";
     private String password="";
@@ -27,9 +29,22 @@ public class SesionActivity extends AppCompatActivity {
         edtPassword = findViewById(R.id.edtPassword);
         btnEntrar = findViewById(R.id.btnEntrar);
         btnEntrar.setOnClickListener(onClickEntrar);
+        txtvRegistrar= findViewById(R.id.txtvRegistrar);
+        txtvRegistrar.setOnClickListener(onClickRegistrar);
         cargarPreferencias();
+        if (Valida())
+        {
+            Intent intMain = new Intent(SesionActivity.this,MainActivity.class );
+            startActivity(intMain);
+        }
 
     }
+
+    View.OnClickListener onClickRegistrar = view ->
+    {
+            Intent intentRegistrar= new Intent(this,RegistroActivity.class);
+            startActivity(intentRegistrar);
+    };
 
     private void cargarPreferencias()
     {
@@ -44,39 +59,45 @@ public class SesionActivity extends AppCompatActivity {
     }
 
 
-    View.OnClickListener onClickEntrar = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if (Valida())
-            {
-                Intent intMain = new Intent(SesionActivity.this,MainActivity.class );
-                startActivity(intMain);
-            }
-            else
-            {
-                Toast.makeText(getApplicationContext(),"Usuario o contraseña incorrecta",Toast.LENGTH_LONG).show();
-            }
-
+    View.OnClickListener onClickEntrar = view -> {
+        if (Valida())
+        {
+            Intent intMain = new Intent(SesionActivity.this,MainActivity.class );
+            startActivity(intMain);
         }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Usuario o contraseña incorrecta",Toast.LENGTH_LONG).show();
+            Log.d("Login","Usuario o contraseña incorrecta " );
+        }
+
     };
 
 
     private boolean Valida()
     {
+        SharedPreferences preferences = getSharedPreferences(getResources().getString(R.string.strCredenciales),Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor= preferences.edit();
+        String user = preferences.getString(getResources().getString(R.string.strUser),getResources().getString(R.string.strNoUsuarios));
+        String passw= preferences.getString(getResources().getString(R.string.strPassword),"");
+
+
         try {
             usuario=edtNombre.getText().toString();
             password= edtPassword.getText().toString();
+            Log.d("User","Usuario: "+ user + " " + usuario);
+            Log.d("Pass","Password: "+passw +" " + password);
         }
         catch (Exception error)
         {
             Log.d("Login","Error en los dados: "+error.toString() );
         }
 
-        if (!password.equals(""))
+        if (password.equals(""))
         {
             return false;
         }
-        else if ( usuario.equals(getResources().getString(R.string.strUser)) && password.equals(getResources().getString(R.string.strPassword))  )
+        else if ( usuario.equals(user) && password.equals(passw) )
         {
             return true;
         }
