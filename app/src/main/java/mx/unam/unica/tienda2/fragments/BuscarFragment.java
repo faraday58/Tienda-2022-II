6e464ecc0bceb17dfeddeb23c9ee026334e201fa
Armando Rodríguez <jarodriguezvera@gmail.com>
2022-03-25 15:01:31 -0600

    imagenes

--- app/src/main/java/mx/unam/unica/tienda2/fragments/BuscarFragment.java
+++ app/src/main/java/mx/unam/unica/tienda2/fragments/BuscarFragment.java
@@ -8,15 +8,43 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import mx.unam.unica.tienda2.R;
+import mx.unam.unica.tienda2.recyclerview.AdaptadorMuestraProducto;
+import mx.unam.unica.tienda2.recyclerview.MuestraProducto;
 
 public class BuscarFragment extends Fragment {
 
+    private RecyclerView rcvProductos;
+    private AdaptadorMuestraProducto mAdapter;
+    private ArrayList<MuestraProducto> productos;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_buscar,container,false);
+        rcvProductos= v.findViewById(R.id.rcvProductos);
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        rcvProductos.setLayoutManager(llm);
+        InicializarAdaptador();
+
            return v;
+
+    }
+
+    private void InicializarAdaptador() {
+        mAdapter= new AdaptadorMuestraProducto(productos);
+        rcvProductos.setAdapter(mAdapter);
+    }
+
+    private void InicializarProductos(){
+        productos= new ArrayList<>();
+        productos.add(new MuestraProducto("Estrena",R.drawable.tenisblancos,"Justo"));
+        productos.add(new MuestraProducto("Estrena",R.drawable.teniscasual,"Justo"));
+        productos.add(new MuestraProducto("Estrena",R.drawable.zapatos,"Justo"));
     }
 }
