530715ed535f159ecdb1cc2bc9509e8e0450681a
Armando <jarodriguezvera@gmail.com>
2022-04-01 14:35:14 -0600

    LoginAuth y Registro

--- app/src/main/java/mx/unam/unica/tienda2/SplashActivity.java
+++ app/src/main/java/mx/unam/unica/tienda2/SplashActivity.java
@@ -2,7 +2,12 @@ package mx.unam.unica.tienda2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.firebase.ui.auth.ui.HelperActivityBase;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        CargarLogin();
+                    }
+                }
+        ,3000);
+
+    }
+
+    private void CargarLogin() {
+        Intent intent = new Intent(this, LoginAuth.class);
+        startActivity(intent);
     }
 }
