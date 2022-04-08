fcc59f32c40c40a184a49c7afdd73410e33dbaeb
Armando <jarodriguezvera@gmail.com>
2022-04-08 14:48:40 -0500

    Firestore Database en clase

--- app/src/main/java/mx/unam/unica/tienda2/fragments/BuscarFragment.java
+++ app/src/main/java/mx/unam/unica/tienda2/fragments/BuscarFragment.java
@@ -11,6 +11,10 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
 import java.util.ArrayList;
 
 import mx.unam.unica.tienda2.R;
@@ -21,16 +25,19 @@ public class BuscarFragment extends Fragment {
 
     private RecyclerView rcvProductos;
     private AdaptadorMuestraProducto mAdapter;
-    private ArrayList<MuestraProducto> productos;
+    private FirebaseFirestore mFirestore;
+ //   private ArrayList<MuestraProducto> productos;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_buscar,container,false);
         rcvProductos= v.findViewById(R.id.rcvProductos);
+        mFirestore= FirebaseFirestore.getInstance();
+
         LinearLayoutManager llm = new LinearLayoutManager(getContext());
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         rcvProductos.setLayoutManager(llm);
-        InicializarProductos();
+//        InicializarProductos();
         InicializarAdaptador();
 
            return v;
@@ -38,14 +45,33 @@ public class BuscarFragment extends Fragment {
     }
 
     private void InicializarAdaptador() {
-        mAdapter= new AdaptadorMuestraProducto(productos);
+        mAdapter= new AdaptadorMuestraProducto(Consulta());
+        mAdapter.notifyDataSetChanged();
         rcvProductos.setAdapter(mAdapter);
     }
 
+    private FirestoreRecyclerOptions Consulta()
+    {
+        Query query = mFirestore.collection("Producto");
+        FirestoreRecyclerOptions<MuestraProducto> firestoreRecyclerOptions = new FirestoreRecyclerOptions.Builder<MuestraProducto>()
+                .setQuery(query,MuestraProducto.class).build();
+        return firestoreRecyclerOptions;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAdapter.startListening(); //Para recargar el rcv cada que seleccionamos el fragment
+    }
+
+    /*
+    Antes de utilizar una base de datos remota
     private void InicializarProductos(){
         productos= new ArrayList<>();
         productos.add(new MuestraProducto("Estrena",R.drawable.tenisblancos,"Justo"));
         productos.add(new MuestraProducto("Discreto",R.drawable.teniscasual,"Momento oportuno"));
         productos.add(new MuestraProducto("Casuales",R.drawable.zapatos,"En familia o el trabajo"));
     }
+ */
+
 }
--- app/src/main/java/mx/unam/unica/tienda2/recyclerview/AdaptadorMuestraProducto.java
+++ app/src/main/java/mx/unam/unica/tienda2/recyclerview/AdaptadorMuestraProducto.java
@@ -9,15 +9,41 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
 import java.util.ArrayList;
 
 import mx.unam.unica.tienda2.R;
 
 
-public class AdaptadorMuestraProducto extends RecyclerView.Adapter<AdaptadorMuestraProducto.ViewHolder> {
+//public class AdaptadorMuestraProducto extends RecyclerView.Adapter<AdaptadorMuestraProducto.ViewHolder> {
+public class AdaptadorMuestraProducto extends FirestoreRecyclerAdapter<MuestraProducto,AdaptadorMuestraProducto.ViewHolder> {
+  /**
+   * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
+   * FirestoreRecyclerOptions} for configuration options.
+   *
+   * @param options
+   */
+  public AdaptadorMuestraProducto(@NonNull FirestoreRecyclerOptions<MuestraProducto> options) {
+    super(options);
+  }
 
+  @Override
+  protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull MuestraProducto producto) {
+    holder.txtCvTitulo.setText(producto.getTitulo() );
+    holder.txtCvDescribe.setText(producto.getDescripcion());
+  }
+
+  @NonNull
+  @Override
+  public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_producto,parent,false);
+    return new ViewHolder(v);
+  }
+
+  /*
   ArrayList<MuestraProducto> productos;
-
   public AdaptadorMuestraProducto(ArrayList<MuestraProducto> productos)
   {
     this.productos= productos;
@@ -41,6 +67,7 @@ public class AdaptadorMuestraProducto extends RecyclerView.Adapter<AdaptadorMues
   public int getItemCount() {
     return productos.size();
   }
+   */
 
   public static class ViewHolder extends RecyclerView.ViewHolder{
     TextView txtCvTitulo;
